e63cf241cd7917497281ea077dcb79bb5ccd27ff
Daniel Skowroński <daniel@dsinf.net>
2021-10-19 20:00:27 +0200

    add support of connection timeouts and retries on failures in _inventory.py

--- _inventory.py
+++ _inventory.py
@@ -2,18 +2,24 @@
 server='http://10.0.7.1:5000'
 import requests,json,sys
 
-try:
-  if len(sys.argv)==2 and sys.argv[1]=='--list':
-    r = requests.get(server+'/ansible/list')
-    if r.status_code!=200:
-      raise Exception('api broken')
-    print(json.dumps(r.json()))
-  elif len(sys.argv)==3 and sys.argv[1]=='--host':
-    r = requests.get(server+'/ansible/host/'+sys.argv[2])
-    if r.status_code!=200:
-      raise Exception('no such host')
-    print(json.dumps(r.json()))
-  else:
-    raise Exception('wrong syntax')
-except Exception as e:
-  print('{"error":"'+str(e)+'"}')
+err = None
+for i in range(0,5):
+  try:
+    if len(sys.argv)==2 and sys.argv[1]=='--list':
+      r = requests.get(server+'/ansible/list', timeout=2)
+      if r.status_code!=200:
+        raise Exception('api broken')
+      print(json.dumps(r.json()))
+      sys.exit(0)
+    elif len(sys.argv)==3 and sys.argv[1]=='--host':
+      r = requests.get(server+'/ansible/host/'+sys.argv[2], timeout=2)
+      if r.status_code!=200:
+        raise Exception('no such host')
+      print(json.dumps(r.json()))
+      sys.exit(0)
+    else:
+      raise Exception('wrong syntax')
+  except Exception as e:
+    err=e
+    pass
+print('{"error":"'+str(err)+'"}')
