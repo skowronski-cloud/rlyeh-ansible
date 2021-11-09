d096ff57ab54675a3f8e1a9165fc4d0003b338a2
Daniel Skowroński <daniel@dsinf.net>
2021-11-09 05:36:23 +0100

    fix issues with accessing fridge dynamic assets in the moment of data download

--- templates/intranet/fridge/fridge_rewriter.sh
+++ templates/intranet/fridge/fridge_rewriter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /srv/fridge/dynamic/
-/usr/bin/curl 'http://api.openweathermap.org/data/2.5/weather?q=Krakow,pl&appid={{ owm_token }}&units=metric' > weather.json
-/usr/bin/curl 'http://api.openweathermap.org/data/2.5/forecast?q=Krakow,pl&appid={{ owm_token }}&units=metric' > forecast.json
-/usr/bin/curl 'https://api.sunrise-sunset.org/json?lat=50.0583674&lng=19.9518081&formatted=0&date='`/bin/date +%Y-%m-%d` > sun.json
+
+/usr/bin/curl 'http://api.openweathermap.org/data/2.5/weather?q=Krakow,pl&appid={{ owm_token }}&units=metric' > weather.json.tmp
+mv weather.json.tmp weather.json
+
+/usr/bin/curl 'http://api.openweathermap.org/data/2.5/forecast?q=Krakow,pl&appid={{ owm_token }}&units=metric' > forecast.json.tmp
+mv forecast.json.tmp forecast.json
+
+/usr/bin/curl 'https://api.sunrise-sunset.org/json?lat=50.0583674&lng=19.9518081&formatted=0&date='`/bin/date +%Y-%m-%d` > sun.json.tmp
+mv sun.json.tmp sun.json
